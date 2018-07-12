ef5ddf72e0a62c67d5e2d57636bc4002ab9e2aa8
gumpy-hybridBCI <38141253+gumpy-hybridBCI@users.noreply.github.com>
2018-07-12 15:15:19 +0100

    Update features.py

--- gumpy/features.py
+++ gumpy/features.py
@@ -7,7 +7,9 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
-
+import numpy as np
+import scipy.linalg as la
+  import pywt
 
 def sequential_feature_selector(features, labels, classifier, k_features, kfold, selection_type, plot=True, **kwargs):
     """Sequential feature selection to reduce the number of features.
@@ -119,8 +121,183 @@ def sequential_feature_selector(features, labels, classifier, k_features, kfold,
 
     return feature_idx, cv_scores, algorithm, sfs, clf
 
+#######################################################################
+
+#Common Spatial Pattern implementation in Python, used to build spatial filters for identifying task-related activity.
 
 
+# CSP takes any number of arguments, but each argument must be a collection of trials associated with a task
+# That is, for N tasks, N arrays are passed to CSP each with dimensionality (# of trials of task N) x (feature vector)
+# Trials may be of any dimension, provided that each trial for each task has the same dimensionality,
+# otherwise there can be no spatial filtering since the trials cannot be compared
+def CSP(*tasks):
+	if len(tasks) < 2:
+		print("Must have at least 2 tasks for filtering.")
+		return (None,) * len(tasks)
+	else:
+		filters = ()
+		# CSP algorithm
+		# For each task x, find the mean variance matrices Rx and not_Rx, which will be used to compute spatial filter SFx
+		iterator = range(0,len(tasks))
+		for x in iterator:
+			# Find Rx
+			Rx = covarianceMatrix(tasks[x][0])
+			for t in range(1,len(tasks[x])):
+				Rx += covarianceMatrix(tasks[x][t])
+			Rx = Rx / len(tasks[x])
+
+			# Find not_Rx
+			count = 0
+			not_Rx = Rx * 0
+			for not_x in [element for element in iterator if element != x]:
+				for t in range(0,len(tasks[not_x])):
+					not_Rx += covarianceMatrix(tasks[not_x][t])
+					count += 1
+			not_Rx = not_Rx / count
+
+			# Find the spatial filter SFx
+			SFx = spatialFilter(Rx,not_Rx)
+			filters += (SFx,)
+
+			# Special case: only two tasks, no need to compute any more mean variances
+			if len(tasks) == 2:
+				filters += (spatialFilter(not_Rx,Rx),)
+				break
+		return filters
+
+# covarianceMatrix takes a matrix A and returns the covariance matrix, scaled by the variance
+def covarianceMatrix(A):
+	#Ca = np.dot(A,np.transpose(A))/np.trace(np.dot(A,np.transpose(A)))
+	Ca = np.cov(A)
+	return Ca
+
+# spatialFilter returns the spatial filter SFa for mean covariance matrices Ra and Rb
+def spatialFilter(Ra,Rb):
+	R = Ra + Rb
+	E,U = la.eig(R)
+
+	# CSP requires the eigenvalues E and eigenvector U be sorted in descending order
+	ord = np.argsort(E)
+	ord = ord[::-1] # argsort gives ascending order, flip to get descending
+	E = E[ord]
+	U = U[:,ord]
+
+	# Find the whitening transformation matrix
+	P = np.dot(np.sqrt(la.inv(np.diag(E))),np.transpose(U))
+
+	# The mean covariance matrices may now be transformed
+	Sa = np.dot(P,np.dot(Ra,np.transpose(P)))
+	Sb = np.dot(P,np.dot(Rb,np.transpose(P)))
+
+	# Find and sort the generalized eigenvalues and eigenvector
+	E1,U1 = la.eig(Sa,Sb)
+	ord1 = np.argsort(E1)
+	ord1 = ord1[::-1]
+	E1 = E1[ord1]
+	U1 = U1[:,ord1]
+
+	# The projection matrix (the spatial filter) may now be obtained
+	SFa = np.dot(np.transpose(U1),P)
+	#return SFa.astype(np.float32)
+	return SFa
+
+############################################################################
+#RMS feature extraction method 
+def RMS_features_extraction(data, trialList, window_size, window_shift):
+    if window_shift > window_size:
+        raise ValueError("window_shift > window_size")
+
+    fs = data.sampling_freq
+    
+    n_features = int(data.duration/(window_size-window_shift))
+    
+    X = np.zeros((len(trialList), n_features*4))
+    
+    t = 0
+    for trial in trialList:
+        # x3 is the worst of all with 43.3% average performance
+        x1=gumpy.signal.rms(trial[0], fs, window_size, window_shift)
+        x2=gumpy.signal.rms(trial[1], fs, window_size, window_shift)
+        x3=gumpy.signal.rms(trial[2], fs, window_size, window_shift)
+        x4=gumpy.signal.rms(trial[3], fs, window_size, window_shift)
+        x=np.concatenate((x1, x2, x3, x4))
+        X[t, :] = np.array([x])
+        t += 1
+    return X
+
+#######################################################
+def dwt_features(data, trials, level, sampling_freq, w, n, wavelet): 
+  
+    
+    # number of features per trial
+    n_features = 9 
+    # allocate memory to store the features
+    X = np.zeros((len(trials), n_features))
+
+    # Extract Features
+    for t, trial in enumerate(trials):
+        signals = data[trial + fs*4 + (w[0]) : trial + fs*4 + (w[1])]
+        coeffs_c3 = pywt.wavedec(data = signals[:,0], wavelet=wavelet, level=level)
+        coeffs_c4 = pywt.wavedec(data = signals[:,1], wavelet=wavelet, level=level)
+        coeffs_cz = pywt.wavedec(data = signals[:,2], wavelet=wavelet, level=level)
+
+        X[t, :] = np.array([
+            np.std(coeffs_c3[n]), np.mean(coeffs_c3[n]**2),  
+            np.std(coeffs_c4[n]), np.mean(coeffs_c4[n]**2),
+            np.std(coeffs_cz[n]), np.mean(coeffs_cz[n]**2), 
+            np.mean(coeffs_c3[n]),
+            np.mean(coeffs_c4[n]), 
+            np.mean(coeffs_cz[n])])
+        
+    return X
+
+##################################################################
+def alpha_subBP_features(data):
+    # filter data in sub-bands by specification of low- and high-cut frequencies
+    alpha1 = gumpy.signal.butter_bandpass(data, 8.5, 11.5, order=6)
+    alpha2 = gumpy.signal.butter_bandpass(data, 9.0, 12.5, order=6)
+    alpha3 = gumpy.signal.butter_bandpass(data, 9.5, 11.5, order=6)
+    alpha4 = gumpy.signal.butter_bandpass(data, 8.0, 10.5, order=6)
+
+    # return a list of sub-bands
+    return [alpha1, alpha2, alpha3, alpha4]
+
+def beta_subBP_features(data):
+    beta1 = gumpy.signal.butter_bandpass(data, 14.0, 30.0, order=6)
+    beta2 = gumpy.signal.butter_bandpass(data, 16.0, 17.0, order=6)
+    beta3 = gumpy.signal.butter_bandpass(data, 17.0, 18.0, order=6)
+    beta4 = gumpy.signal.butter_bandpass(data, 18.0, 19.0, order=6)
+    return [beta1, beta2, beta3, beta4]
+
+def powermean(data, trial, fs, w):
+    return np.power(data[trial+fs*4+w[0]: trial+fs*4+w[1],0],2).mean(), \
+           np.power(data[trial+fs*4+w[0]: trial+fs*4+w[1],1],2).mean(), \
+           np.power(data[trial+fs*4+w[0]: trial+fs*4+w[1],2],2).mean()
+
+def log_subBP_feature_extraction(alpha, beta, trials, fs, w):
+    # number of features combined for all trials
+    n_features = 15
+    # initialize the feature matrix
+    X = np.zeros((len(trials), n_features))
+    
+    # Extract features
+    for t, trial in enumerate(trials):
+        power_c31, power_c41, power_cz1 = powermean(alpha[0], trial, fs, w)
+        power_c32, power_c42, power_cz2 = powermean(alpha[1], trial, fs, w)
+        power_c33, power_c43, power_cz3 = powermean(alpha[2], trial, fs, w)
+        power_c34, power_c44, power_cz4 = powermean(alpha[3], trial, fs, w)
+        power_c31_b, power_c41_b, power_cz1_b = powermean(beta[0], trial, fs, w)
+        
+        X[t, :] = np.array(
+            [np.log(power_c31), np.log(power_c41), np.log(power_cz1),
+             np.log(power_c32), np.log(power_c42), np.log(power_cz2),
+             np.log(power_c33), np.log(power_c43), np.log(power_cz3), 
+             np.log(power_c34), np.log(power_c44), np.log(power_cz4),
+             np.log(power_c31_b), np.log(power_c41_b), np.log(power_cz1_b)])
+
+    return X
+
+#########################################################################################################
 
 def PCA_dim_red(features, var_desired):
     """Dimensionality reduction of features using PCA.
